140cc030fc65c31eda13a12e99f25c5d3519f659
Ordaz Romero Sergio <orcheko@gmail.com>
2022-09-01 00:50:59 -0500

    Lanzador de dados terminado

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var firstDice: UIImageView!
+    @IBOutlet var secondDice: UIImageView!
+    
+    let rolls = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        firstDice.image = rolls.randomElement()
+        secondDice.image = rolls.randomElement()
     }
 
-
+    @IBAction func rollButtonAction(_ sender: Any) {
+//        Se puede hacer uso de números aleatorios o un randomElement
+//        let firstRoll: Int = Int.random(in: 0...5)
+//        let secondRoll: Int = Int.random(in: 0...5)
+        firstDice.image = rolls.randomElement()
+        secondDice.image = rolls.randomElement()
+//        array.shuffle() aleatoriza los elementos de l arreglo
+    }
+    
 }
 
